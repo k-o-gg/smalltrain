16b272f9b41de865ed0a6187a7104311e9244fa8
k-o-gg <kogawa.gg@gmail.com>
2020-11-27 10:43:00 +0900

    Classify CIFAR100 image(class8: bear) and renewal labels new label 0: CIFAR100 class8(bear) new label 1: CIFAR10 labels 3,4,5,7(cat,deer,dog,horse) new label 2: CIFAR10 labels 0,1,8,9(airplane,automobile,ship,truck)

--- tutorials/image_recognition/convert_cifar_data_set.py
+++ tutorials/image_recognition/convert_cifar_data_set.py
@@ -11,11 +11,13 @@ CODE_TRAIN = 0  # data_set_def code for train data
 CODE_TEST = 1  # data_set_def code for test data
 
 CIFAR_10_URL = 'https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz'
+CIFAR_100_URL = 'https://www.cs.toronto.edu/~kriz/cifar-100-python.tar.gz'
 
 ROOT_DIR_PATH = '/var/data/'
 WORK_DIR_PATH = os.path.join(ROOT_DIR_PATH, 'work')
 SRC_DATA_DIR = os.path.join(ROOT_DIR_PATH, 'cifar-10-batches-py')
 DIST_DATA_DIR = os.path.join(ROOT_DIR_PATH, 'cifar-10-image')
+SRC_100_DATA_DIR = os.path.join(ROOT_DIR_PATH, 'cifar-100-python')
 
 '''
 1. Downloads cifar10 data from
@@ -47,12 +49,12 @@ DIST_DATA_DIR
 └── test_batch    // testing data
 '''
 
-def download_cifar_10():
-    file_name = CIFAR_10_URL.split('/')[-1]
+def download_cifar_data(url):
+    file_name = url.split('/')[-1]
     file_path_to_download = os.path.join(WORK_DIR_PATH, file_name)
     if not os.path.isfile(file_path_to_download):
         import requests
-        downloaded_file = requests.get(CIFAR_10_URL)
+        downloaded_file = requests.get(url)
         os.makedirs(WORK_DIR_PATH, exist_ok=True)
         open(file_path_to_download, 'wb').write(downloaded_file.content)
         print('Downloaded: {}'.format(file_path_to_download))
@@ -61,7 +63,6 @@ def download_cifar_10():
 
     extract_tarfile(file_path_to_download, ROOT_DIR_PATH)
     print('Extracted tarfile: {}'.format(file_path_to_download))
-
 
 def extract_tarfile(file_path, path_to_extract=None):
     if path_to_extract is None:
@@ -120,11 +121,15 @@ def export_data_set_def(df_src_array):
 
 if __name__ == '__main__':
 
-    download_cifar_10()
-
+    download_cifar_data(CIFAR_10_URL)
+    download_cifar_data(CIFAR_100_URL)
+    
     data_batch_list = ['data_batch_1', 'data_batch_2', 'data_batch_3', 'data_batch_4', 'data_batch_5', 'test_batch']
 
     all_file_list = []
+
+    cifar100_image_data_set = unpickle(os.path.join(SRC_100_DATA_DIR, 'train'))
+    c100_images = cifar100_image_data_set[b'data'].reshape(50000, 3, 32, 32).transpose(0,2,3,1).astype('uint8')
 
     for data_batch in data_batch_list:
 
@@ -135,17 +140,26 @@ if __name__ == '__main__':
 
         labels = np.array(image_data_set[b'labels'])
         images = image_data_set[b'data'].reshape(10000, 3, 32, 32).transpose(0,2,3,1).astype('uint8')
+        file_num = 0
+        # new label 1: CIFAR10 labels 3,4,5,7(cat,deer,dog,horse) new label 2: CIFAR10 labels 0,1,8,9(airplane,automobile,ship,truck)
+        label_dict = {0:2,1:2,3:1,4:1,5:1,7:1,8:2,9:2}
 
         for i, image_array in enumerate(images):
+            
+            if labels[i] in label_dict:
+                label = label_dict[labels[i]]
+            else:
+                continue
+            
             image = Image.fromarray(image_array)
-            label = labels[i]
-            file_name = '{}_i{}_c{}.png'.format(data_batch, i, label)
+            file_name = '{}_i{}_c{}.png'.format(data_batch, file_num, label)
             file_path = os.path.join(dir_path, file_name)
             image.save(file_path)
             all_file_list.append(file_path)
+            file_num += 1
 
             # assertion
-            if i == 0:
+            if file_num == 1:
                 read_image_array = np.asarray(Image.open(file_path))
                 assert read_image_array.shape == image_array.shape
                 assert math.fabs(float(read_image_array[0][0][0] - image_array[0][0][0]) < 1e-4)
@@ -153,7 +167,31 @@ if __name__ == '__main__':
                 read_class_num = get_class_num_from_file_path(file_path)
                 print('read_class_num:{}, label:{}'.format(read_class_num, label))
                 assert read_class_num == label
+           
+        if data_batch == 'test_batch':
+            cifar100_image_data_set = unpickle(os.path.join(SRC_100_DATA_DIR, 'test'))
+            c100_images = cifar100_image_data_set[b'data'].reshape(10000, 3, 32, 32).transpose(0,2,3,1).astype('uint8')
+        
+        coarse_labels = cifar100_image_data_set[b'coarse_labels']
+        fine_labels = cifar100_image_data_set[b'fine_labels']
+        file_num = 90000
+        target_file_count = 0
+        for i, image_array in enumerate(c100_images):
+            if coarse_labels[i] != 8 or fine_labels[i] != 3:
+                continue
+            target_file_count += 1
+            if data_batch == 'test_batch' or ((int(data_batch[-1]) - 1)*100 < target_file_count and target_file_count <= (int(data_batch[-1]))*100 ):
+                print('image add')
+                image = Image.fromarray(image_array)
+                label = 0
+                file_name = '{}_i{}_c{}.png'.format(data_batch, file_num , label)
+                file_path = os.path.join(dir_path, file_name)
+                image.save(file_path)
+                all_file_list.append(file_path)
+                file_num += 1
 
+            if (data_batch != "test_batch" and target_file_count == int(data_batch[-1])*100) or (data_batch == "test_batch" and target_file_count == 100):
+                break
 
     # prepare data_set_def
     all_label_list = [get_class_num_from_file_path(file_path) for file_path in all_file_list]
